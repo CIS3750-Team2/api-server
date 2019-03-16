92b7b19d2921e98c94deb48e6e114771e2108442
EvilKanoa <kanoa@kanoa.ca>
2019-03-16 01:17:28 -0400

    Added fetching from 1996 to 2017 for ontario data service

--- src/services.js
+++ src/services.js
@@ -6,17 +6,28 @@ module.exports = {
         {
             province: 'ontario',
             enabled: true,
-            years: [2017, 2016],
+            years: [
+                2017, 2016, 2015, 2014,
+                2013, 2012, 2011, 2010,
+                2009, 2008, 2007, 2006,
+                2005, 2004, 2003, 2002,
+                2001, 2000, 1999, 1998,
+                1997, 1996
+            ],
         },
         {
             province: 'alberta',
             enabled: false,
-            years: [2018],
+            years: [
+                2017
+            ],
         },
         {
             province: 'quebec',
             enabled: false,
-            years: [2018],
+            years: [
+                2017
+            ],
         },
     ]
 };
